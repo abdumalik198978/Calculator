9f7632f30a88089d9988d674226d870a9237c3cd
abdumalikkholmirzaev <abdumalik1989@yahoo.com>
2020-02-05 14:39:53 -0600

    adde subtract method

--- src/calculator.java
+++ src/calculator.java
@@ -1,6 +1,9 @@
 public class calculator {
-    public static double sum(double a, double b){
+    public static double add(double a, double b){
         return a+b;
+    }
+    public static double subtract(double c, double d){
+return c-d;
     }
 
 }
