18f05c92b070e19aaeba14448b80c2520b21c31d
abdumalikkholmirzaev <abdumalik1989@yahoo.com>
2020-02-05 22:32:45 -0600

    try to add

--- src/calculator.java
+++ src/calculator.java
@@ -1,7 +1,7 @@
 public class calculator {
     public static double add(double a, double b){
         return a+b;
-    }
+    }//ooo
     //very useful method
     public static double subtract(double c, double d){
 return c-d;
